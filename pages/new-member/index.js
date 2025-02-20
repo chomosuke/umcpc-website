a5067703245d329933a9e9b96a6be6abea2e5d76
troppydash <troppydash.developer@gmail.com>
2025-02-20 17:11:59 +1100

    Added new-member page

---
+++ pages/new-member/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+
+const NewMember = () => {
+  useEffect(() => {
+    window.location.replace(
+      'https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/6517/'
+    )
+  }, [])
+  return (
+    <div className="h-screen relative z-0">
+      <span className="text-white">
+        {' '}
+        Click{' '}
+        <a
+          className="font-bold underline"
+          href="https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/6517/"
+        >
+          here
+        </a>{' '}
+        if the redirect is not automatic
+      </span>
+    </div>
+  )
+}
+
+export default NewMember
